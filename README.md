29453b5bb81dcb197112f0951120d15575914c1d
Dominic Muttel <dominic.muttel@gmail.com>
2016-12-15 16:02:38 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# exercismIO-exercises
---
+++ javascript/leap/leap.js
@@ -0,0 +1,17 @@
+function Leap(year) {
+  this.year = year;
+};
+
+Leap.prototype.isLeap = function(year) {
+  if (this.year % 4 != 0) {
+    return false;
+  }
+
+  if (this.year % 100 == 0 && this.year % 400 != 0) {
+    return false;
+  }
+
+  return true;
+}
+
+module.exports = Leap;
